76bc8795d2ea427782caac545af157c80607fffc
JoanaLP <63454701+JoanaLP@users.noreply.github.com>
2020-07-21 12:05:12 +0100

    Add files via upload

---
+++ Rlearning.R
@@ -0,0 +1,64 @@
+2+2
+4+3
+3+3+3+3
+data()
+data("AirPassengers")
+AirPassengers
+mean(AirPassengers)
+median(AirPassengers)
+summary(AirPassengers)
+Jan1949<-112
+Jan1949=112
+rm(Jan1949)
+help("anova")
+Jan1950=115
+Jan1960=417
+Jan1950
+Jan1960
+rm(Jan1950)
+rm(Jan1960)
+Jan1960
+Jan1950<-115
+Jan1960<-417
+Jan1950
+Jan1960
+Jan1960-Jan1950
+rm(Jan1950,Jan1960)
+Air49<- Jan1949
+Air49 <- c(Jan1949)
+Air49 <- c(112,118,132,129,121,135,148,148,136,119,104,118)
+Air49[1:5]
+Air49[5:7]
+rm(Air49)
+Air49 <- AirPassengers[1:12]
+Air50 <- AirPassengers[13:24]
+mean(Air49)
+sum(Air49)
+help(sum)
+Air49
+sum(Air50)
+Air49+Air50
+Sum(Air49+Air50)
+sum <- Air49+Air50
+Airtotal <- (sum(Air49)+(Air50))
+Airtotal
+sum(Air49,Air50)
+data("mtcars")
+help("mtcars")
+mtcars[1,1]
+mtcars[24,5]
+mtcars[1,]
+summary(mtcars[,1])
+mtcars
+mean(mtcars[,6])
+max(mtcars[,7])
+summary(mtcars)
+mtcars[29,4]-mtcars[9,4]
+install.packages("readxl")
+library(readxl)
+help("readxl")
+write.csv(mtcars,file="cars.csv")
+install.packages("neuropsychology")
+library(neuropsychology)
+help("neuropsychology")
+help(neuropsychology)
